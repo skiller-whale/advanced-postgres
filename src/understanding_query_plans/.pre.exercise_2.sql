68a002c1885fbda089036f8eae4b96c32126e889
Hywel Carver <>
2024-02-21 17:18:14 +0000

    Update from source

--- src/understanding_query_plans/.pre.exercise_2.sql
+++ src/understanding_query_plans/.pre.exercise_2.sql
@@ -6,3 +6,9 @@ ALTER TABLE work_items SET (autovacuum_enabled = false);
 SELECT pg_sleep(1);
 
 DELETE FROM pg_statistic;
+
+-- This removes statistics about rows
+-- see https://www.postgresql.org/docs/current/planner-stats.html
+UPDATE pg_class
+SET reltuples=0, relpages=0
+WHERE relname='work_items';
